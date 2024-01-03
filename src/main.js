4e293ca45d0b885efd7f821acad08c082badc8cf
ArneSamson <arnesamson007@gmail.com>
2024-01-03 14:22:27 +0100

    feat: add model for poses and for images

--- src/main.js
+++ src/main.js
@@ -1,41 +1,78 @@
+// More API functions here:
+    // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
 
-const URL = "../src/model/";
+    // the link to your model provided by Teachable Machine export panel
+    const poseModelURL = "../src/poseModel/";
+    const imageModelURL = "../src/imageModel/";
+    let model, webcam, ctx, labelContainer, labelName, labelConfidence, maxPredictions;
 
-let model, labelContainer, maxPredictions;
+    labelContainer = document.getElementById("label");
+    labelName = document.getElementById("label-name");
+    labelPrediction = document.getElementById("label-confidence");
 
-async function init() {
-    const modelURL = URL + "model.json";
 
-    // load the model
-    model = await tf.loadLayersModel(modelURL);
 
-    // setup the file picker
-    const uploadFile = document.getElementById("upload-file");
-    uploadFile.addEventListener("change", function (event) {
-        const files = event.target.files;
-        if (files.length > 0) {
-            const file = files[0];
-            if (URL.createObjectURL(file)) {
-                document.getElementById("myImg").src = URL.createObjectURL(file);
+    async function init() {
+        const modelURL = URL + "model.json";
+        const metadataURL = URL + "metadata.json";
+
+        // load the model and metadata
+        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+        // Note: the pose library adds a tmPose object to your window (window.tmPose)
+        model = await tmPose.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
+
+        // Convenience function to setup a webcam
+        const size = 200;
+        const flip = true; // whether to flip the webcam
+        webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
+        await webcam.setup(); // request access to the webcam
+        await webcam.play();
+        window.requestAnimationFrame(loop);
+
+        // append/get elements to the DOM
+        const canvas = document.getElementById("canvas");
+        canvas.width = size; canvas.height = size;
+        ctx = canvas.getContext("2d");
+    }
+
+    async function loop(timestamp) {
+        webcam.update(); // update the webcam frame
+        await predict();
+        window.requestAnimationFrame(loop);
+    }
+
+    async function predict() {
+        // Prediction #1: run input through posenet
+        // estimatePose can take in an image, video or canvas html element
+        const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
+        // Prediction 2: run input through teachable machine classification model
+        const prediction = await model.predict(posenetOutput);
+
+        let highestPrediction = { className: "", probability: 0 };
+        for (let i = 0; i < maxPredictions; i++) {
+            if (prediction[i].probability > highestPrediction.probability) {
+                highestPrediction = prediction[i];
             }
         }
-    });
-}
 
-async function predict() {
-    // get the image element
-    const imgElement = document.getElementById("myImg");
+        // const classPrediction = highestPrediction.className + ": " + highestPrediction.probability.toFixed(2);
+        labelName.innerHTML = highestPrediction.className + " ";
+        //percentage of prediction
+        labelPrediction.innerHTML = (highestPrediction.probability * 100).toFixed(0) + "% ";
+        // finally draw the poses
+        drawPose(pose);
+        // console.log(highestPrediction.className);
+    }
 
-    // convert the image to a tensor
-    let img = tf.browser.fromPixels(imgElement);
-    img = img.expandDims(0);
-
-    // predict can take in a tensor
-    const prediction = await model.predict(img);
-
-    // get the class with the highest probability
-    const classId = prediction.argMax(-1).dataSync()[0];
-
-    // display the class prediction
-    labelContainer.innerHTML = "Class: " + classId;
-}
+    function drawPose(pose) {
+        if (webcam.canvas) {
+            ctx.drawImage(webcam.canvas, 0, 0);
+            // draw the keypoints and skeleton
+            if (pose) {
+                const minPartConfidence = 0.5;
+                tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+                tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+            }
+        }
+    }
